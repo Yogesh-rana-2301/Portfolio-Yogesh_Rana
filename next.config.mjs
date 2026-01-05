5c44aa07af74e8a966d007475161a6d9ea3fc819
Yogesh-rana-2301 <yogeshrana2301@gmail.com>
2026-01-06 00:27:20 +0530

    deploy fix 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/Portfolio-Yogesh_Rana",
+  output: 'export',
+  basePath: isGitHubPages ? '/Portfolio-Yogesh_Rana' : '',
   trailingSlash: true,
   images: {
     unoptimized: true,
